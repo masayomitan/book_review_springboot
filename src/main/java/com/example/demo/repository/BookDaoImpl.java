1a0cd6c9f0876ab6e41c926f4a59f6ca5d1687bb
kawasaki <yomitanmasa3523@gmail.com>
2020-06-15 12:19:46 +0900

    repository 処理

--- src/main/java/com/example/demo/repository/BookDaoImpl.java
+++ src/main/java/com/example/demo/repository/BookDaoImpl.java
@@ -70,6 +70,45 @@ public class BookDaoImpl implements BookDao {
  }
 
 
+ @Override
+ public Optional<Book> findById(int id){
 
+  String sql = "SELECT book.id, title, author, publisher, buyDate, releaseDate, overView "
+  + "WHERE book.id = ?";
+
+  Map<String, Object> result = jdbcTemplate.queryForMap(sql, id);
+
+  Book book = new Book();
+			book.setId((int)result.get("id"));
+			book.setTitle((String)result.get("title"));
+			book.setAuthor((String)result.get("author"));
+			book.setPublisher((String)result.get("publisher"));
+      book.setBuyDate((Date)result.get("buyDate"));
+      book.setReleaseDate((Date)result.get("releaseDate"));
+      book.setOverView((String)result.get("overView"));
+      book.setDeadline(((Timestamp) result.get("deadline")).toLocalDateTime());
+
+    //taskをOptionalでラップする
+		Optional<Book> taskOpt = Optional.ofNullable(book);
+		return taskOpt;
+ }
+
+
+ @Override
+	public void insert(Book book) {
+		jdbcTemplate.update("INSERT INTO book(id, title, author, publisher, buyDate, releaseDate, overView, deadline) VALUES(?, ?, ?, ?, ?, ?, ?, ?)",
+				 book.getId(), book.getTitle(), book.getAuthor(), book.getPublisher(), book.getBuyDate(), book.getReleaseDate(), book.getOverView(), book.getDeadline() );
+	}
+	
+	@Override
+	public int update(Book book) {
+		return 	jdbcTemplate.update("UPDATE book SET id = ?, title = ?, author = ?, publisher = ?, buyDate = ?, releaseDate = ?, overView = ?, deadline = ? WHERE id = ?",
+    book.getId(), book.getTitle(), book.getAuthor(), book.getPublisher(), book.getBuyDate(), book.getReleaseDate(), book.getOverView(), book.getDeadline() );
+}
+
+	@Override
+	public int deleteById(int id) {
+		return jdbcTemplate.update("DELETE FROM book WHERE id = ?", id);
+	}
 
 }
