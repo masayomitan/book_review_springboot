9327f87386a0ea84ca04df3521f258ceb304f87e
kawasaki <yomitanmasa3523@gmail.com>
2020-06-21 23:02:13 +0900

    new

--- src/main/java/com/example/demo/controller/bookController.java
+++ src/main/java/com/example/demo/controller/bookController.java
@@ -10,7 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 
 
@@ -26,10 +28,18 @@ public class BookController {
 
      //Taskのリストを取得する
      List<Book> book = bookService.findAll();
-
+     
     model.addAttribute("book", book);
     return "books/index";
-    
   }
+
+
+
+  @GetMapping("new")
+  public String newBook(@ModelAttribute("book") Book book, Model model) {
+      return "books/new";
+  }
+
+	
 
 }
--- src/main/java/com/example/demo/repository/BookRepositoryImpl.java
+++ src/main/java/com/example/demo/repository/BookRepositoryImpl.java
@@ -51,45 +51,47 @@ public class BookRepositoryImpl implements BookRepository{
       return list;
     }
 
+
+
     @Override
- public Optional<Book> findOne(int id){
+    public Optional<Book> findOne(int id){
 
-  String sql = "SELECT book.id, title, author, publisher, buyDate, releaseDate, overView FROM Book"
-  + "WHERE book.id = ?";
+      String sql = "SELECT book.id, title, author, publisher, buyDate, releaseDate, overView FROM Book"
+      + "WHERE book.id = ?";
 
-  Map<String, Object> result = jdbcTemplate.queryForMap(sql, id);
+      Map<String, Object> result = jdbcTemplate.queryForMap(sql, id);
 
-  Book book = new Book();
-			book.setId((int)result.get("id"));
-			book.setTitle((String)result.get("title"));
-			book.setAuthor((String)result.get("author"));
-			book.setPublisher((String)result.get("publisher"));
-      book.setBuyDate((Date)result.get("buyDate"));
-      book.setReleaseDate((Date)result.get("releaseDate"));
-      book.setOverView((String)result.get("overView"));
-      book.setDeadline(((Timestamp) result.get("deadline")).toLocalDateTime());
+      Book book = new Book();
+          book.setId((int)result.get("id"));
+          book.setTitle((String)result.get("title"));
+          book.setAuthor((String)result.get("author"));
+          book.setPublisher((String)result.get("publisher"));
+          book.setBuyDate((Date)result.get("buyDate"));
+          book.setReleaseDate((Date)result.get("releaseDate"));
+          book.setOverView((String)result.get("overView"));
+          book.setDeadline(((Timestamp) result.get("deadline")).toLocalDateTime());
 
-    //taskをOptionalでラップする
-		Optional<Book> taskOpt = Optional.ofNullable(book);
-		return taskOpt;
- }
+        //taskをOptionalでラップする
+        Optional<Book> taskOpt = Optional.ofNullable(book);
+        return taskOpt;
+    }
 
 
-  @Override
-	public void save(Book book) {
-		jdbcTemplate.update("INSERT INTO book(id, title, author, publisher, buyDate, releaseDate, overView, deadline) VALUES(?, ?, ?, ?, ?, ?, ?, ?)",
-				 book.getId(), book.getTitle(), book.getAuthor(), book.getPublisher(), book.getBuyDate(), book.getReleaseDate(), book.getOverView(), book.getDeadline() );
-	}
-	
-	@Override
-	public int update(Book book) {
-		return 	jdbcTemplate.update("UPDATE book SET id = ?, title = ?, author = ?, publisher = ?, buyDate = ?, releaseDate = ?, overView = ?, deadline = ? WHERE id = ?",
-    book.getId(), book.getTitle(), book.getAuthor(), book.getPublisher(), book.getBuyDate(), book.getReleaseDate(), book.getOverView(), book.getDeadline() );
-}
+      @Override
+      public void save(Book book) {
+        jdbcTemplate.update("INSERT INTO book(id, title, author, publisher, buyDate, releaseDate, overView, deadline) VALUES(?, ?, ?, ?, ?, ?, ?, ?)",
+            book.getId(), book.getTitle(), book.getAuthor(), book.getPublisher(), book.getBuyDate(), book.getReleaseDate(), book.getOverView(), book.getDeadline() );
+      }
+      
+      @Override
+      public int update(Book book) {
+        return 	jdbcTemplate.update("UPDATE book SET id = ?, title = ?, author = ?, publisher = ?, buyDate = ?, releaseDate = ?, overView = ?, deadline = ? WHERE id = ?",
+        book.getId(), book.getTitle(), book.getAuthor(), book.getPublisher(), book.getBuyDate(), book.getReleaseDate(), book.getOverView(), book.getDeadline() );
+    }
 
-	@Override
-	public int delete(int id) {
-		return jdbcTemplate.update("DELETE FROM book WHERE id = ?", id);
-	}
+      @Override
+      public int delete(int id) {
+        return jdbcTemplate.update("DELETE FROM book WHERE id = ?", id);
+      }
 
-}
+    }
